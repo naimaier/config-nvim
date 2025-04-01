2bf5c7d3de9a28fc690d314f83659d9c6588703c
Henrique Naimaier <coubron@yahoo.com.br>
2025-04-01 17:20:13 -0300

    First commit

---
+++ lua/core/plugins.lua
@@ -0,0 +1,32 @@
+require("lazy").setup({
+  { "folke/tokyonight.nvim", priority = 1000 },
+  "tpope/vim-commentary",
+  "mattn/emmet-vim",
+  "nvim-lualine/lualine.nvim",
+  "nvim-treesitter/nvim-treesitter",
+  "vim-test/vim-test",
+  "lewis6991/gitsigns.nvim",
+  "tpope/vim-surround",
+  -- completion
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "L3MON4D3/LuaSnip",
+  "saadparwaiz1/cmp_luasnip",
+  "rafamadriz/friendly-snippets",
+  "github/copilot.vim",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+  {
+    "vinnymeller/swagger-preview.nvim",
+    run = "npm install -g swagger-ui-watcher",
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+  },
+  {
+    "nvim-telescope/telescope.nvim", tag = "0.1.4",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+})
