ccb75ccc9c1eaa1452b2932bb526599b497fbf23
Henrique Naimaier <coubron@yahoo.com.br>
2025-12-18 16:30:46 -0300

    add harpoon

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -15,6 +15,19 @@ keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
+-- Harpoon
+keymap.set("n", "<leader>ha", require("harpoon.mark").add_file)
+keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu)
+keymap.set("n", "<leader>h1", function() require("harpoon.ui").nav_file(1) end)
+keymap.set("n", "<leader>h2", function() require("harpoon.ui").nav_file(2) end)
+keymap.set("n", "<leader>h3", function() require("harpoon.ui").nav_file(3) end)
+keymap.set("n", "<leader>h4", function() require("harpoon.ui").nav_file(4) end)
+keymap.set("n", "<leader>h5", function() require("harpoon.ui").nav_file(5) end)
+keymap.set("n", "<leader>h6", function() require("harpoon.ui").nav_file(6) end)
+keymap.set("n", "<leader>h7", function() require("harpoon.ui").nav_file(7) end)
+keymap.set("n", "<leader>h8", function() require("harpoon.ui").nav_file(8) end)
+keymap.set("n", "<leader>h9", function() require("harpoon.ui").nav_file(9) end)
+
 -- LSP
 keymap.set('n', '<leader>gg', '<cmd>lua vim.lsp.buf.hover()<CR>')
 keymap.set('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
